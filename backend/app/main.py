eec5e73c9fb8f97bee12c3eab4abc8b0b3343ecf
Anuja Rawat <anujarawat13@gmail.com>
2026-08-05 19:54:05 +0530

    fix cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -22,6 +22,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
+        "https://repo-mind-72jltqnkd-anujarawr.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
